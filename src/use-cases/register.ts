103027164aedbd424ee279b9b5fbed6379d41fa9
MatDev435 <matheuspsalmazo2@gmail.com>
2024-04-28 16:34:07 -0300

    Testes unitários - Hash da senha no registro do usuário

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/prisma/users-repository'
 import { hash } from 'bcryptjs'
 import { EmailAlreadyExistsError } from './errors/email-already-exists'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseProps {
   name: string
@@ -8,10 +9,18 @@ interface RegisterUseCaseProps {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseProps) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseProps): Promise<RegisterUseCaseResponse> {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
@@ -20,10 +29,12 @@ export class RegisterUseCase {
 
     const passwordHash = await hash(password, 6)
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash: passwordHash,
     })
+
+    return { user }
   }
 }

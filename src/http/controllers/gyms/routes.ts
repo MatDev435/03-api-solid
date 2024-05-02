b5462aedb90166ec4cbe3bfdd1f25ac168467e97
MatDev435 <matheuspsalmazo2@gmail.com>
2024-05-02 11:12:07 -0300

    Controllers e testes E2E - Controllers para busca de academias pelo nome e busca de academias próximas

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,14 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { search } from './search'
+import { nearby } from './nearby'
 
 export async function gymRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 
   app.post('/gyms', create)
 }
